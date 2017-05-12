6cfb27786cb1eeef729d513502ceb0a442b72d12
voyager.woo <voyager.woo@gmail.com>
2017-05-13 02:34:37 +0900

    rename

--- src/main/java/com/voyager/rxstudy/e08/E08Application.java
+++ src/main/java/com/voyager/rxstudy/e08/E08Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
