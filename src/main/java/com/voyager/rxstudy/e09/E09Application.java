c829e61144047a115b49be1779c7510feb3c99d9
voyager.woo <voyager.woo@gmail.com>
2017-05-19 20:05:06 +0900

    rename

---
+++ src/main/java/com/voyager/rxstudy/e09/E09Application.java
@@ -0,0 +1,72 @@
+package com.voyager.rxstudy.e09;
+
+import io.netty.channel.nio.NioEventLoopGroup;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.Netty4ClientHttpRequestFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.AsyncRestTemplate;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.concurrent.Future;
+
+@SpringBootApplication
+@EnableAsync
+public class E09Application {
+	@RestController
+	static class Controller {
+		AsyncRestTemplate rt = new AsyncRestTemplate(new Netty4ClientHttpRequestFactory(new NioEventLoopGroup(1)));
+
+		@Autowired MyService myService;
+
+		@GetMapping("/rest")
+		public DeferredResult<String> rest(int  idx) throws Exception {
+			DeferredResult<String> dr = new DeferredResult<>();
+			ListenableFuture<ResponseEntity<String>> lf1 = rt.getForEntity("http://localhost:7071/service?req={req}", String.class, "Hello" + idx);
+
+			lf1.addCallback(s-> {
+				ListenableFuture<ResponseEntity<String>> lf2 = rt.getForEntity("http://localhost:7071/service2?req={req}", String.class, "Hello" + s.getBody());
+
+				lf2.addCallback(s2-> {
+					ListenableFuture<String> lf3 = myService.work(s2.getBody());
+					lf3.addCallback(s3-> {
+						dr.setResult(s3);
+					}, e->dr.setErrorResult(e.getMessage()));
+				}, e-> dr.setErrorResult(e.getMessage()));
+			}, e-> dr.setErrorResult(e.getMessage()));
+			return dr;
+		}
+	}
+
+	@Service
+	static class MyService {
+		@Async
+		public ListenableFuture<String> work(String req) {
+			return new AsyncResult<>(req + "/asyncwork");
+		}
+	}
+
+	@Bean
+	ThreadPoolTaskExecutor myThreadPool() {
+		ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
+		te.setCorePoolSize(1);
+		te.setMaxPoolSize(10);
+		te.initialize();
+		return te;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(E09Application.class, args);
+	}
+}
---
+++ src/main/java/com/voyager/rxstudy/e09/RemoteService.java
@@ -0,0 +1,32 @@
+package com.voyager.rxstudy.e09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class RemoteService {
+	@RestController
+	static class Controller {
+		@GetMapping("/service")
+		public String service(String req) throws Exception {
+			TimeUnit.SECONDS.sleep(2);
+			return req + "/service";
+		}
+
+		@GetMapping("/service2")
+		public String service2(String req) throws Exception {
+			TimeUnit.SECONDS.sleep(2);
+			return req + "/service2";
+		}
+	}
+
+	public static void main(String[] args) {
+		System.setProperty("SERVER_PORT", "7071");
+		System.setProperty("server.tomcat.max-threads", "1000");
+		SpringApplication.run(RemoteService.class, args);
+	}
+}
